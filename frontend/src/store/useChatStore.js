bfbb03cb9784dae147a9aea4c5b9b690dcb396fd
MaanyaJ <maanyajaladi@gmail.com>
2025-04-15 20:23:38 +0530

    useChatStore update

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -48,19 +48,23 @@ export const useChatStore = create ((set, get) => ({
     },
 
     subscribeToMessages: () => {
-      const { selectedUser } = get()
-      if(!selectedUser) return;
-
+      const { selectedUser } = get();
+      if (!selectedUser) return;
+    
       const socket = useAuthStore.getState().socket;
-
+    
       socket.on("newMessage", (newMessage) => {
-        const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
-        if(isMessageSentFromSelectedUser) return;
-        set({
-          messages: [...get().messages, newMessage],
-        })
-      })
+        const isFromOrToSelectedUser =
+          newMessage.senderId === selectedUser._id || newMessage.receiverId === selectedUser._id;
+    
+        if (isFromOrToSelectedUser) {
+          set({
+            messages: [...get().messages, newMessage],
+          });
+        }
+      });
     },
+    
 
     unsubscribeFromMessages: () => {
       const socket = useAuthStore.getState().socket;
